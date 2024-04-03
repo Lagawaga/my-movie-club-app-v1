e76dc47e4f1cc131d891863d95579f3c25601c21
Lagawaga <kamronjahani@gmail.com>
2024-04-02 20:42:54 -0400

    took out firebaseconfig info

--- src/firebase.js
+++ src/firebase.js
@@ -9,16 +9,7 @@ import {
   query,
   updateDoc,
 } from "firebase/firestore/lite";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDgdu5_gt8unUPB1sNzM1M4r5OqS5Q5-1g",
-  authDomain: "movie-app-2f6bf.firebaseapp.com",
-  projectId: "movie-app-2f6bf",
-  storageBucket: "movie-app-2f6bf.appspot.com",
-  messagingSenderId: "1074280876139",
-  appId: "1:1074280876139:web:dd92fea9e7934786d7c92e",
-  measurementId: "G-ZTE442HJJM",
-};
+import { firebaseConfig } from "./firebaseconfig.js";
 
 const firebaseApp = initializeApp(firebaseConfig);
 
@@ -47,22 +38,19 @@ export const createMovie = async (movie) => {
 
 export const deleteMovie = async (movieId) => {
   try {
-    // Query the movies collection to find the document with the given movie ID
     const moviesQuery = query(
       collection(db, "movies"),
       where("id", "==", movieId)
     );
     const querySnapshot = await getDocs(moviesQuery);
 
-    // Iterate through the query results
     querySnapshot.forEach((doc) => {
-      // Delete the movie document
       deleteDoc(doc.ref);
       console.log("Movie deleted successfully");
     });
   } catch (error) {
     console.error("Error deleting movie:", error);
-    throw error; // Re-throw the error to handle it in the calling code
+    throw error;
   }
 };
 
@@ -75,7 +63,7 @@ export const markAsWatched = async (movieId) => {
     const querySnapshot = await getDocs(moviesQuery);
     querySnapshot.forEach((doc) => {
       updateDoc(doc.ref, {
-        watched: 1, // Set the watched property to 1
+        watched: 1,
       });
 
       console.log("Movie marked as watched successfully.");
@@ -93,6 +81,6 @@ export const getAllMovies = async () => {
     return movies;
   } catch (error) {
     console.error("Error fetching movies:", error);
-    return []; // Return empty array in case of error
+    return [];
   }
 };
